864bfb0cfb49ea4553779cd93d83db11e80ff5aa
Zoa Hickenlooper <z@vx.gay>
2026-06-25 07:52:19 -0400

    Parse Incus instances into discovered services

--- crates/blackwall-discovery/src/lib.rs
+++ crates/blackwall-discovery/src/lib.rs
@@ -3,10 +3,12 @@
 
 mod error;
 mod host;
+mod incus_model;
 mod proc_io;
 mod reconcile;
 
 pub use error::DiscoveryError;
 pub use host::{parse_proc_net, ListeningSocket};
+pub use incus_model::{instance_services, parse_instance, parse_ports, Instance};
 pub use proc_io::scan_host_sockets;
 pub use reconcile::{reconcile, DiscoveredService, DiscoverySource};
