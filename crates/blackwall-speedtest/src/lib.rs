4dad9840706c1268bf9d1b0aa4d99e48732cbc06
Zoa Hickenlooper <z@vx.gay>
2026-06-25 09:10:47 -0400

    Aggregate provider readings with a trimmed mean

--- crates/blackwall-speedtest/src/lib.rs
+++ crates/blackwall-speedtest/src/lib.rs
@@ -1,10 +1,12 @@
 //! Multi-source speedtest aggregator for Blackwall.
 
+mod aggregate;
 mod error;
 mod provider;
 mod reading;
 mod throughput;
 
+pub use aggregate::aggregate;
 pub use error::SpeedtestError;
 pub use provider::{SpeedtestConfig, SpeedtestProvider};
 pub use reading::{Aggregate, ProviderReading};
