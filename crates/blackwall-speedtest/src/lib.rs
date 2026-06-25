8ced700579ef47acfa26c91cdf6706593825c43d
Zoa Hickenlooper <z@vx.gay>
2026-06-25 09:13:41 -0400

    Add concurrent speedtest runner tolerant of failures

--- crates/blackwall-speedtest/src/lib.rs
+++ crates/blackwall-speedtest/src/lib.rs
@@ -4,10 +4,12 @@ mod aggregate;
 mod error;
 mod provider;
 mod reading;
+mod runner;
 mod throughput;
 
 pub use aggregate::aggregate;
 pub use error::SpeedtestError;
 pub use provider::{SpeedtestConfig, SpeedtestProvider};
 pub use reading::{Aggregate, ProviderReading};
+pub use runner::Speedtest;
 pub use throughput::{mbps_from, trimmed_mean};
